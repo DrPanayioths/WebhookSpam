ec382a99a6d58acc6956b300722c4b9689844fb5
DrPanayioths <panayioths0072019@gmail.com>
2023-07-08 09:53:58 +0300

    Update InstaHack.py

--- InstaHack.py
+++ InstaHack.py
@@ -11,15 +11,18 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install',
 ## Url Of The Webhook
 url = 'Add Your Webhook URL'
 
-
+## Input The Text To System For Testing Purpose
 drp = input("Enter The Text" + ":")
 contentofspam = {'content': drp}
 
+## Print Credits
 print("Made By DrPanayioths For Testing Purposes")
 time.sleep(5)
 
+## Protocol How Many Times You Want To Spam The Webhook
 spamprotocol = input("How Many Times You Want To Spam The Webhook?")
 
+## Change From String To Integer
 spamprotocol = int(spamprotocol)
 
 ## Change The Number after the range for the Spam
