299310541406026aab0d938714974627ce86bdf3
DrPanayioths <panayioths0072019@gmail.com>
2024-09-22 16:41:21 +0300

    Visual Updates, Digits Check
    Visual Updates like CMD Text Color and Spaces between some inputs. Also a check that check if spamprotocol have letters in the answer of the user

--- Webhook.py
+++ Webhook.py
@@ -1,24 +1,36 @@
 import requests
+import os
+import sys
+import time
+
+os.system('color e')
 
 ## URL Of Webhook
-url = 'Change This To Your Discord Webhook URL'
-
+url = 'https://discord.com/api/webhooks/1208816366501634068/G-jqa_q82W_fSFQLSo1DehGYRkuH8bng9-DuTeK51OhR3Vn1tpfmG1P0GxTs8qBtlqIO'
+# Change This To Your Discord Webhook URL
 ## Capture Of Text
 text = input("Enter The Text" + ": ")
 contentofspam = {'content': text}
 
 ## Protocol How Many Times You Want To Spam The Webhook
 spamprotocol = input("How Many Times You Want To Spam The Webhook?: ")
+print("")
 
-## Change From String To Integer
-spamprotocol = int(spamprotocol)
+## Check For Letters in Spam Protocol
+if spamprotocol.isdigit():
+    number_requests = 0
+    for x in range(int(spamprotocol)):
+        x  = requests.post(url, json = contentofspam,)
+        number_requests += 1
+        print(str(number_requests) + " Messages Transmitted")
+else:
+    print("")
+    print("Only Numbers Are Allowed")
+    time.sleep(2)
+    sys.exit()
 
-## Change The Number after the range for the Spam
-number_requests = 0
-for x in range(int(spamprotocol)):
-    x  = requests.post(url, json = contentofspam,)
-    number_requests += 1
-    print(str(number_requests) + " Messages Transmitted")
+
+
     
     
     
